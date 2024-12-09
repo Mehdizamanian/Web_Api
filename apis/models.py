4689df488f99e7a789bcba50eb31161983958a0d
Mehdizamanian <zamanianmehdi23@gmail.com>
2024-12-09 11:40:45 +0330

    book model created not makemigrations

--- apis/models.py
+++ apis/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
+class Book(models.Model):
+  title=models.CharField(max_length=250)
+  subtitle=models.CharField(max_length=250)
+  author=models.CharField(max_length=250)
+  isbn=models.CharField(max_length=250)
+  #my extrafields
+  description=models.TextField(blank=True,null=True)
+  updated_time=models.DateTimeField( auto_now=True, auto_now_add=False)
+  created_time=models.DateTimeField( auto_now=False, auto_now_add=True)
+  status=models.BooleanField(default=True)
+
+  def __str__(self):
+    return self.title
+
+
 # Create your models here.

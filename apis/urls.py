000fdf98366698492138b400c126e6cec005e44e
Mehdizamanian <zamanianmehdi23@gmail.com>
2024-12-09 13:45:00 +0330

    Crude created by  function apiview

--- apis/urls.py
+++ apis/urls.py
@@ -3,13 +3,21 @@ APIS APP
 """
 
 from django.urls import path
-from . import views
+from . import views 
 
 app_name="apis"
 
 urlpatterns = [
 
   path('', views.index,name="index"),
+
+  # api views
+  path('api/', views.book_list,name="book_list"),
+  path('api/detail/<pk>/', views.book_detail,name="book_detail"),
+
+  path('api/create/', views.book_create,name="book_create"),
+  path('api/edit/<pk>/', views.book_update,name="book_update"),
+  path('api/delete/<pk>/', views.book_delete,name="book_delete"),
 
 ]
 
--- apis/views.py
+++ apis/views.py
@@ -2,11 +2,65 @@
 from django.shortcuts import render , HttpResponse
 from .models import Book
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import BookSerializer
 
+
+# jinja views
 
 def index(request):
   books=Book.objects.all()
   context={'books':books}
   return render(request,'apis/index.html',context)
 
-# Create your views here.
+
+
+
+# Api views
+
+@api_view(['GET'])
+def book_list(request):
+    books=Book.objects.all()
+    books_serializer=BookSerializer(books,many=True)
+    return Response(books_serializer.data)
+
+
+@api_view(['GET'])
+def book_detail(request,pk):
+  book=Book.objects.get(id=pk)
+  book_serializer=BookSerializer(book,many=False)
+  return Response(book_serializer.data)
+
+
+
+
+
+
+
+@api_view(["POST"])
+def book_create(request):
+  serialized_data=BookSerializer(data=request.data)
+  if serialized_data.is_valid():
+    serialized_data.save()
+  return Response(serialized_data.data)
+
+
+
+@api_view(['POST'])
+def book_update(request,pk):
+   instance=Book.objects.get(id=pk)
+   book_serializer=BookSerializer(instance=instance,data=request.data)
+   if book_serializer.is_valid():
+      book_serializer.save()
+   return Response(book_serializer.data)
+
+
+
+
+
+@api_view(["DELETE"])
+def book_delete(request,pk):
+   instance=Book.objects.get(id=pk)
+   instance.delete()
+   return Response("book deleted successfully .")

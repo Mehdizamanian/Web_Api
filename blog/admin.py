4546e9202bb94368e54c8df9f0b68fe81bdcb45b
Mehdizamanian <zamanianmehdi23@gmail.com>
2024-12-10 12:18:58 +0330

    admin blog sonfigured

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Post  , Category , Comment ,Tag
+
+
+class PostAdmin(admin.ModelAdmin):
+  list_display=['id','title','created_time','updated_time','active',]
+  list_filter=['active']
+  date_hierarchy='created_time'
+  search_fields=('title','brif_description','description',)
+  list_display_links=['id','title']
+
+admin.site.register(Post,PostAdmin)
+
+
+admin.site.register(Category)
+
+admin.site.register(Tag)
+
+class CommentAdmin(admin.ModelAdmin):
+  list_display=['email','post','created_time','active',]
+
+admin.site.register(Comment,CommentAdmin)
 
 # Register your models here.
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,74 @@
 from django.db import models
 
-# Create your models here.
+
+
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+
+user=get_user_model()
+
+
+class Post(models.Model):
+  title=models.CharField(max_length=250)
+  brif_description=models.TextField()
+  description=models.TextField()
+  image=models.ImageField(upload_to='posts/images/',default='posts/images/1.png')
+  updated_time=models.DateTimeField(auto_now=True, auto_now_add=False)
+  created_time=models.DateTimeField(auto_now=False, auto_now_add=True)
+  active=models.BooleanField(default=False)
+  # relations
+  author=models.ForeignKey(user,on_delete=models.CASCADE,blank=False)
+  category=models.ManyToManyField("Category")
+  tag=models.ManyToManyField("Tag")
+  # slug=
+
+  def __str__(self) -> str:
+    return self.title
+
+
+  class Meta:
+    ordering=['-created_time']
+    verbose_name = 'Post'
+    verbose_name_plural = 'Posts'
+
+
+
+
+
+
+
+class Category(models.Model):
+  title=models.CharField(max_length=100)
+  
+  def __str__(self):
+    return self.title
+  
+
+
+
+class Comment(models.Model):
+  post=models.ForeignKey(Post, on_delete=models.CASCADE)
+  name=models.CharField(max_length=100)
+  email=models.EmailField(max_length=254)
+  message=models.TextField()
+  active=models.BooleanField(default=False)
+  created_time=models.DateField( auto_now_add=True)
+
+
+  def __str__(self):
+    return self.name
+
+
+  class Meta:
+    ordering=['-created_time',]
+
+
+
+
+class Tag(models.Model):
+  title=models.CharField(max_length=100)
+  
+  def __str__(self):
+    return self.title

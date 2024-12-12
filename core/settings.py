930e24173c945945d16036b183b75b37a200237f
Mehdizamanian <zamanianmehdi23@gmail.com>
2024-12-12 11:32:40 +0330

    sessionAuthrntication used

--- core/settings.py
+++ core/settings.py
@@ -60,7 +60,8 @@ REST_FRAMEWORK = {
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
       
-        'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
 
     ]
 
